44f636d57bc98ee06da99f1cf73dff8a9196dd8a
StrixPO <rusarrp@gmail.com>
2023-05-21 20:51:25 +0545

    sec_commit

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    books_title = Book.objects.all()
+    return render(request, 'book_outlet/book_list.html', {'books': books})
+
+
+def index(request):
+    return render(request, "book_outlet/index.html")

232d9d73da5547d07e269d4a8bbc8b1780e81f18
StrixPO <rusarrp@gmail.com>
2023-05-30 20:57:49 +0545

    database_complete

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.index),
-    path("books/", views.book_list)
+    path("books/", views.book_list),
+    path("<slug:slug>", views.book_detail, name="book-detail")
 
-}
+]
--- book_store/urls.py
+++ book_store/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from book_outlet.views import book_list
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'),
+
     path('admin/', admin.site.urls),
     path('', include("book_outlet.urls"))
 ]

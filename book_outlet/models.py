2f1f3b833ae025a10570e53f64633a528713affc
StrixPO <rusarrp@gmail.com>
2023-05-31 16:51:16 +0545

    admin_config

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -14,9 +14,6 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(args, **kwargs)
-    
+
     def __str__(self):
         return f"{self.title} ({self.rating})"
